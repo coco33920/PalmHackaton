92e196b5defa365a8f8ba63b72d0552ad4b48ff7
Jaggernaute <lucas.videcoq1@gmail.com>
2023-04-05 14:36:19 +0200

    :sparkles: init server

---
+++ server/src/main/java/gay/bacoin/Server.java
@@ -0,0 +1,18 @@
+package gay.bacoin;
+
+import static spark.Spark.*;
+
+public class Server {
+    public static void main(String[] args) {
+        ipAddress("127.0.0.1");
+        port(8080);
+        setupRoute();
+    }
+
+    private static void setupRoute(){
+        get("/", (request, response) -> {
+            response.type("application/json");
+            return "{\"text\":\"Hello World\"}";
+        });
+    }
+}
