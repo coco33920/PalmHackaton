afe79b6126048cd76fb160c1a6972227c2035eb9
Jaggernaute <lucas.videcoq1@gmail.com>
2023-04-07 23:52:31 +0200

    Add room size

--- server/src/main/java/gay/bacoin/game/tiles/Room.java
+++ server/src/main/java/gay/bacoin/game/tiles/Room.java
@@ -3,14 +3,18 @@ package gay.bacoin.game.tiles;
 import gay.bacoin.game.Player;
 
 import java.util.ArrayList;
-import java.util.List;
 
 public class Room extends Tile {
 
+    private int height;
+    private int width;
+
     private ArrayList<Player> occupancy = new ArrayList<>();
 
-    public Room(int x, int y) {
+    public Room(int x, int y, int height, int width) {
         super(x, y);
+        this.height = height;
+        this.width = width;
     }
 
     public void addPlayyer(Player player) {
