fbb3e2043271b2f12758f81967f0e624d0f89597
Charlotte Thomas <contact@nwa2coco.fr>
2023-04-08 01:43:44 +0200

    fix error

--- server/src/main/java/gay/bacoin/game/Weapons.java
+++ server/src/main/java/gay/bacoin/game/Weapons.java
@@ -1,41 +1,36 @@
 package gay.bacoin.game;
 
-import java.util.ArrayList;
+public enum Weapons {
+    BEC(0, "bec benzène"),
+    BROYEUR(1, "Broyeur"),
+    CHALUMEAU(2, "Chalumeau"),
+    RM(3, "rm -rf memoire.pdf"),
+    GRIBOUILLAGE(4, "Gribouillage dessus"),
+    CAFE(5, "Café"),
+    WINDOWS(6, "Windows failure"),
+    MARTEAU(7, "Marteau");
+    ;
 
-public class Weapons {
+    private final String text;
+    private final int id;
 
-      public enum weapons {
-          BEC(0, "bec benzène"),
-          BROYEUR(1, "Broyeur"),
-          CHALUMEAU(2, "Chalumeau"),
-          RM(3, "rm -rf memoire.pdf"),
-          GRIBOUILLAGE(4, "Gribouillage dessus"),
-          CAFE(5, "Café"),
-          WINDOWS(6, "Windows failure"),
-          MARTEAU(7, "Marteau");
-          ;
+    /**
+     * @param text
+     */
+    Weapons(final int id, final String text) {
+        this.id = id;
+        this.text = text;
+    }
 
-            private final String text;
-            private final int id;
+    /* (non-Javadoc)
+     * @see java.lang.Enum#toString()
+     */
+    @Override
+    public String toString() {
+        return text;
+    }
 
-            /**
-             * @param text
-             */
-            weapons(final int id, final String text) {
-                this.id = id;
-                this.text = text;
-            }
-
-            /* (non-Javadoc)
-             * @see java.lang.Enum#toString()
-             */
-            @Override
-            public String toString() {
-              return text;
-            }
-
-          public int getId() {
-              return id;
-          }
-      }
+    public int getId() {
+        return id;
+    }
 }
