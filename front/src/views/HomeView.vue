940aea55788e1d0e7b31f93a103cb0e02dc9cd17
Baptiste <baptiste.corbet@gmail.com>
2023-04-08 01:44:18 +0200

    added cell generation for the board

--- front/src/views/HomeView.vue
+++ front/src/views/HomeView.vue
@@ -1,20 +1,24 @@
 <script>
 export default {
   setup() {
-    console.log("yes 🫁")
-  },
+    console.log('hi mom')
+  }
 }
 </script>
 
-
 <template>
   <main>
-    <img id="board" src="@/assets/board.webp"/>
+    <div class="board-wrapper">
+      <div class="board">
+        <div class="board-line" v-for="i in 24 * 24" :key="i" :index = "i" />
+      </div>
+      <img id="board-bg" src="@/assets/board.webp" />
+    </div>
   </main>
 </template>
 
 <style scoped>
-#board {
+#board-bg {
   height: 99vh;
   aspect-ratio: 1/1;
 }
