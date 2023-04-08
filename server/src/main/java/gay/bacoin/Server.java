e4fd994b61b6667e2586e0581bff3e070bd1ee03
Charlotte Thomas <contact@nwa2coco.fr>
2023-04-08 02:20:51 +0200

    allow cors

--- server/src/main/java/gay/bacoin/Server.java
+++ server/src/main/java/gay/bacoin/Server.java
@@ -17,15 +17,41 @@ public class Server {
     }
 
     private static void setupRoute() {
+
+        options("/*", (request, response) -> {
+
+            String accessControlRequestHeaders = request.headers("Access-Control-Request-Headers");
+            if (accessControlRequestHeaders != null) {
+                response.header("Access-Control-Allow-Headers", accessControlRequestHeaders);
+            }
+
+            String accessControlRequestMethod = request.headers("Access-Control-Request-Method");
+            if (accessControlRequestMethod != null) {
+                response.header("Access-Control-Allow-Methods", accessControlRequestMethod);
+            }
+
+            return "OK";
+        });
+
+        before((request, response) -> {
+            response.header("Access-Control-Allow-Origin", "*");
+            response.header("Access-Control-Request-Method", "GET, POST, PUT, DELETE, OPTIONS");
+            response.header("Access-Control-Allow-Headers", "Content-Type, Authorization, Content-Length, X-Requested-With");
+            // Note: this may or may not be necessary in your particular application
+            response.type("application/json");
+        });
+
         get("/", (request, response) -> {
             response.type("application/json");
             return "{\"text\":\"Hello World\"}";
         });
+
         get("/request_new_game", (request, response) -> {
             UUID uuid = Game.generateNewGame();
             response.type("application/json");
             return "{\"uuid\":\"" + uuid + "\"}";
         });
+
         get("/game_state/:uuid", (request, response) -> {
             response.type("application/json");
             String uuid = request.params(":uuid");
@@ -35,6 +61,7 @@ public class Server {
                 return "{}";
             return new Gson().toJson(g.getPlayerList());
         });
+
         post("/move_player/:uuid", (request, response) -> {
             response.type("application/json");
             String uuid = request.params(":uuid");
@@ -46,6 +73,7 @@ public class Server {
             boolean success = g.movePlayer(movePlayerRequest);
             return "{\"success\":" + success + "}";
         });
+
         post("/check_guess/:uuid", (request, response) -> {
             response.type("application/json");
             String uuid = request.params(":uuid");
